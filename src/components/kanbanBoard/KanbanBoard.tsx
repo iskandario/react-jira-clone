03bbd46d52a9407d99ab3911bcc68b4e123f89ab
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-04 20:52:56 +0000

    Store functionality to add new card

--- src/components/kanbanBoard/KanbanBoard.tsx
+++ src/components/kanbanBoard/KanbanBoard.tsx
@@ -1,19 +1,42 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../../features/backlogList'
 import KanbanBreadcrumbs from './KanbanBreadcrumbs'
 import MainTitleArea from './MainTitleArea'
 import KanbanTopMenu from './KanbanTopMenu'
 import KanbanMainBoard from './KanbanMainBoard'
 import NewTaskItem from './NewTaskItem'
+import EditTaskItem from './EditTaskItem'
+
+interface taskType {
+    description: string,
+    type: string,
+    priority: number,
+    user: string,
+}
 
 const KanbanBoard = () => {
-    const showNewTaskCard = false;
+    const [ showNewTaskCard, setShowTaskCard ] = useState(false);
+    const [ showEditTaskCard, setShowEditTaskCard ] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const addTask = (task: taskType) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+
+        const newTask = { id, ...task };
+
+        dispatch(addItem(newTask));
+    }
 
     return (
         <div className='kanbanMainArea'>
             <KanbanBreadcrumbs />
             <MainTitleArea title='Kanban Board' buttonText='Github Repo' />
-            <KanbanTopMenu />
-            <KanbanMainBoard />
-            { showNewTaskCard && <NewTaskItem /> }
+            <KanbanTopMenu onOpen={() => setShowTaskCard(true)} />
+            <KanbanMainBoard onOpen={() => setShowEditTaskCard(true)} />
+            { showNewTaskCard && <NewTaskItem onClose={() => setShowTaskCard(false)} onAdd={addTask} /> }
+            { showEditTaskCard && <EditTaskItem onClose={() => setShowEditTaskCard(false)}  /> }
         </div>
     )
 }
