cdf30bce4d1f82f7db1fa1eb967afa91754f2ee7
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-19 20:18:46 +0000

    Updated store with new types

--- src/features/backlogList.ts
+++ src/features/backlogList.ts
@@ -1,46 +1,105 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TaskItem } from "../types/mainTypes";
 
 const initialState = {
   value: [
     {
-      description: "This is an issue of type: Task",
+      title: "Front End Beta Version",
+      description: "",
       id: 1,
       type: "task",
       user: "EJ",
       priority: 1,
+      code: "MET-14",
+      status: "todo",
+      main_details: {
+        description: "",
+        environment: "",
+        user_stories: "",
+        affected_areas: "",
+      },
+      side_details: {
+        user: "",
+        priority: null,
+        reporter: "",
+        labels: "",
+        project_manager: "",
+        test: "",
+        client: "",
+        effort: "",
+        start_date: "",
+        chargable: "",
+        fail_count: null,
+      },
     },
     {
-      description: "Second type of task to action today",
+      title: "BE Core work on Mongo",
+      description: "",
       id: 2,
       type: "task",
       user: "EJ",
       priority: 4,
+      code: "PEP-66",
+      status: "todo",
+      main_details: {
+        description: "",
+        environment: "",
+        user_stories: "",
+        affected_areas: "",
+      },
+      side_details: {
+        user: "",
+        priority: null,
+        reporter: "",
+        labels: "",
+        project_manager: "",
+        test: "",
+        client: "",
+        effort: "",
+        start_date: "",
+        chargable: "",
+        fail_count: null,
+      },
     },
     {
-      description: "Main story for issue task to be completed first",
+      title: "Main story for issue task to be completed first",
+      description: "",
       id: 1,
       type: "story",
       user: "Ewa",
       priority: 2,
+      code: "PEP-51",
+      status: "todo",
+      main_details: {
+        description: "",
+        environment: "",
+        user_stories: "",
+        affected_areas: "",
+      },
+      side_details: {
+        user: "",
+        priority: null,
+        reporter: "",
+        labels: "",
+        project_manager: "",
+        test: "",
+        client: "",
+        effort: "",
+        start_date: "",
+        chargable: "",
+        fail_count: null,
+      },
     },
   ],
 };
-
-interface logItem {
-  description: string;
-  id: number;
-  type: string;
-  user: string;
-  priority: number;
-}
 
 export const backlogList = createSlice({
   name: "backlogList",
   initialState,
   reducers: {
     // methods to update state
-    addItem: (state, action: PayloadAction<logItem>) => {
-      console.log("new item:", action.payload);
+    addItem: (state, action: PayloadAction<TaskItem>) => {
+      console.log("new item payload:", action.payload);
       state.value.push(action.payload);
     },
   },
