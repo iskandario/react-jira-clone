04990c98ae805c5f2a7b245c5358f8c5fa9f6b41
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-25 15:19:42 +0000

    Added bug to list of ticket types

--- src/components/kanbanBoard/KanbanCard.tsx
+++ src/components/kanbanBoard/KanbanCard.tsx
@@ -6,30 +6,41 @@ interface kanbanCardTypes {
     title: string,
     description: string,
     user: string,
+    priority: string,
     type: string,
-    id: number,
-    priority: number
+    id: number
 }
 
 const KanbanCard = (cardItem: kanbanCardTypes)  => {
 
-    const arrowColour = (priority: number) => {
+    const arrowColour = (priority: string) => {
         switch (priority) {
-            case 1:
+            case '1':
                 return { color: '#FF5630'}
-            case 2:
+            case '2':
                 return { color: '#FFAB00'}
-            case 3:
+            case '3':
                 return { color: '#FFC400'}
-            case 4:
+            case '4':
                 return { color: '#36B37E'}
-            case 5:
+            case '5':
                 return { color: '#2684FF'}
         }
     }
 
+    const ticketColor = (type: string) => {
+      switch(type) {
+        case 'task': 
+          return { borderLeft: '2px solid #4C9AFF' }
+        case 'story': 
+          return {borderLeft: '2px solid #36B37E' }
+        case 'bug': 
+          return {borderLeft: '2px solid #FF5630' }
+      }
+    }
+
     return (
-        <div className='kanbanCardArea' style={cardItem.type === 'task' ? {  borderLeft: '2px solid #4C9AFF'} :  {borderLeft: '2px solid #36B37E' }} >
+        <div className='kanbanCardArea' style={ticketColor(cardItem.type)} >
             <p>{ cardItem.title }</p>
             <div className='kanbanCardBottom'>
                 <div className='kanbanCardBottom'>
@@ -37,7 +48,7 @@ const KanbanCard = (cardItem: kanbanCardTypes)  => {
                         { cardItem.type === 'task' ? <IoMdCheckbox style={{ color: '#4C9AFF', fontSize: '15px'}} /> : <BsFillBookmarkFill style={{ color: '#36B37E' }}/> }
                     </div>
                     <div style={{ marginLeft: '10px', fontSize: '14px' }}>
-                        { cardItem.priority > 3 ? <AiOutlineArrowUp style={arrowColour(cardItem.priority)} /> : <AiOutlineArrowDown style={arrowColour(cardItem.priority)}/> }
+                        { cardItem.priority > '3' ? <AiOutlineArrowUp style={arrowColour(cardItem.priority)} /> : <AiOutlineArrowDown style={arrowColour(cardItem.priority)}/> }
                     </div>
                 </div>
                 <div>{ cardItem.user }</div>
