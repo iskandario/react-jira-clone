d9f77fae786fed175bdb0855125bd8512497517b
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-25 15:19:28 +0000

    Added input width prop

--- src/components/simpleComponents/FormTextInput.tsx
+++ src/components/simpleComponents/FormTextInput.tsx
@@ -2,15 +2,17 @@
 interface inputInfo {
     label: string,
     inputValue: any,
+    inputType: string,
+    inputWidth: string,
     setChange: any
 }
 
-const FormTextInput = ({ label, inputValue, setChange }: inputInfo) => {
+const FormTextInput = ({ label, inputValue, inputType, inputWidth, setChange }: inputInfo) => {
     return (
 
         <div >
             <h5>{label}</h5>
-            <input className='formTextInput' value={inputValue} onChange={(e) => setChange(e.target.value)}  />
+            <input style={inputWidth === 'full' ? { width:'100%'} : {}} className='formTextInput' type={inputType} value={inputValue} onChange={(e) => setChange(e.target.value)}  />
         </div>
 
     )
